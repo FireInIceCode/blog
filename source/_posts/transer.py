7e5976456e61726c0c2d2622a352f4484a936bc4
fireinice <miaoxingren2006@163.com>
2022-11-07 22:01:17 +0800

    update

--- source/_posts/transer.py
+++ source/_posts/transer.py
@@ -1,27 +1,30 @@
-import os,re
+import os
+import re
 
+markrule=('，。：；！？￥（）【】“‘《》',',.:;!?$()[]\"\'<>')
 
 def translatexmark(s):
     ls=list(s)
     inlatex=False
     for i,c in enumerate(s):
         if c=='$' and (i==len(s)-1 or s[i+1]!='$') and (i==0 or s[i-1]!='$'):
-            if not inlatex and (i==0 or s[i-1] not in (' ','\n') and ls[i+1]!=' '):
+            if not inlatex and (i==0 or s[i-1] not in (' ','\n',*markrule) and ls[i+1]!=' '):
                 ls[i]=' $'
             if not inlatex and s[i+1]==' ':
                 ls[i+1]=''
-            if inlatex and (i==len(s)-1 or s[i+1] not in (' ','\n') and ls[i+1]!=' '):
+            if inlatex and (i==len(s)-1 or s[i+1] not in (' ','\n',*markrule) and ls[i+1]!=' '):
                 ls[i]='$ '
             if inlatex and ls[i-1][-1]==' ':
                 ls[i-1]=''
             inlatex=not inlatex
+        if c=='|' and inlatex:
+            ls[i]=' \\vert '
     s=''.join(ls)
     s=s.replace('\n\n$$\n\n','\n$$\n\n')
     return s
 
 def transdot(s):
-    rule=('，。：；！？￥（）【】“‘《》',',.:;!?$()[]\"\'<>')
-    for a,b in zip(rule[0],rule[1]):
+    for a,b in zip(markrule[0],markrule[1]):
         s=s.replace(a,b)
     return s
 
